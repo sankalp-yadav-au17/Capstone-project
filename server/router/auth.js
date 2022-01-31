dba64e24b92faeb975ce71b9dfa865aeb2f807af
sankalp-yadav-au17 <sankalp_ma@srmuniv.edu.in>
2022-01-31 22:15:16 +0530

    a

--- server/router/auth.js
+++ server/router/auth.js
@@ -8,9 +8,9 @@ const authenticate = require("../middleware/authenticate");
 require('../db/conn');
 const User = require("../model/userSchema");
 
-router.get('/', (req, res) => {
-    res.send(`Hello world from the server rotuer js`);
-});
+// router.get('/', (req, res) => {
+//     res.send(`Hello world from the server rotuer js`);
+// });
 
 
 
